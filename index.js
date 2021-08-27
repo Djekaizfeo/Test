72f29b57b4ccb4f2c630d199d59ea3ff9e608784
Djekaizfeo <Djekaizfeo-05@mail.ru>
2021-08-28 00:04:42 +0300

    Add parrot image, shaking buttons, new buttons colours

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const buttonElement = document.querySelector(".button1");
+const buttonsElement = document.querySelector(".buttons")
 
 const buttonHandler = (evt) => {
-    document.body.append(cloneElement(evt.target, "wuewue"));
-    document.body.prepend(cloneElement(evt.target, "wuewue"));
+    buttonsElement.append(cloneElement(evt.target, "wuewue"));
+    buttonsElement.prepend(cloneElement(evt.target, "wuewue"));
+    
+    if (!document.querySelector(".jezzImage")) {
+        document.body.append(createImage());
+    }
 
     document.querySelector("audio").play();
+    evt.target.classList.add("shake");
 };
 
 const cloneElement = (oldElement, textInsert) => {
     const newElement = oldElement.cloneNode();
     
     newElement.textContent = textInsert;
+    newElement.classList.add("button2");
 
     return newElement;
+};
+
+const createImage = () => {
+    const imageElement = document.createElement("img");
+
+    imageElement.src = "jezz.jpg";
+    imageElement.classList.add("jezzImage");
+
+    return imageElement;
 };
 
 buttonElement.addEventListener("click", buttonHandler);
